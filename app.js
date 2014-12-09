0356c72c788eddb36f86678409044d4bcd14442b
Chad Jewsbury <cjewsbury@groupon.com>
2014-12-08 21:44:22 -0800

    Finished step4

--- app.js
+++ app.js
@@ -5,15 +5,14 @@
         Collections: {},
         Views: {},
 
-        start: function () {
-            console.log("STarted");
-    // var contacts = new ContactManager.Collections.Contacts(data.contacts);
+        start: function (data) {
+            var contacts = new ContactManager.Collections.Contacts(data.contacts);
 
-    // var contactsView = new ContactManager.Views.Contacts({
-    //   collection: contacts
-    // });
+            var contactsView = new ContactManager.Views.Contacts({
+              collection: contacts
+            });
 
-    // $('.main-container').html(contactsView.render().$el);
+            $('.main-container').html(contactsView.render().$el);
         }
     };
 }());
--- app/js/views/contact.js
+++ app/js/views/contact.js
@@ -1,8 +1,10 @@
 ContactManager.Views.Contact = Backbone.View.extend({
+  tagName: 'li',
+  className: 'media col-md-6 col-lg-4',
+  template: _.template($('#tpl-contact').html()),
   render: function() {
-    var html = '<h1>' + this.model.get('name') + '</h1>';
+    var html = this.template(this.model.toJSON());
     this.$el.html(html);
     return this;
   }, 
-  tagName: 'div'
-})
+});
