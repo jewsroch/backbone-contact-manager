860cd314ef1d5bb44dcda78127b96b2283dec87a
Chad Jewsbury <cjewsbury@groupon.com>
2014-12-08 20:25:49 -0800

    My step 4 attempt

---
+++ app.js
@@ -0,0 +1,19 @@
+(function () {
+    "use strict";
+    window.ContactManager = {
+        Models: {},
+        Collections: {},
+        Views: {},
+
+        start: function () {
+            console.log("STarted");
+    // var contacts = new ContactManager.Collections.Contacts(data.contacts);
+
+    // var contactsView = new ContactManager.Views.Contacts({
+    //   collection: contacts
+    // });
+
+    // $('.main-container').html(contactsView.render().$el);
+        }
+    };
+}());
---
+++ app/js/views/contact.js
@@ -0,0 +1,8 @@
+ContactManager.Views.Contact = Backbone.View.extend({
+  render: function() {
+    var html = '<h1>' + this.model.get('name') + '</h1>';
+    this.$el.html(html);
+    return this;
+  }, 
+  tagName: 'div'
+})
